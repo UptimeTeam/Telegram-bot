44594b75c117b0ff0a98d123ef916c6dfc343245
UptimeTeam <shaukerovaa68@gmail.com>
2024-11-08 21:36:47 +0500

    //

--- venv/main.py
+++ venv/main.py
@@ -4,28 +4,24 @@ import telebot
 bot = telebot.TeleBot('7933512901:AAFgV8RvDQH7_0UEceoMDynmunZwBBTO_MM')
 
 # вывод на команду старт
-
+    
 
 @bot.message_handler(commands=['start'])
 def main(message):
     # создаем клавиатуруруру
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button_corpuses = types.KeyboardButton("🏢Корпуса ТИУ")
-    button_ask_question = types.KeyboardButton(
-        "❓️Задать вопрос")
-    markup.add(button_corpuses, button_ask_question)  # добавляем новые кнопки
+    button_ask_question = types.KeyboardButton("❓️Задать вопрос")
+    button_spravka = types.KeyboardButton("Справка")
+    keyboard.add(button_corpuses, button_ask_question, button_spravka)  # добавляем новые кнопки
     bot.send_message(message.chat.id, 'Привет!\n\n🤖 "Студенческий Помощник" — ваш надежный спутник в мире учебы! '
                      'Этот бот создан для того, чтобы облегчить жизнь студентам. Он быстро отвечает на часто задаваемые '
                      'вопросы о расписании, экзаменах, учебных материалах и студенческой жизни.\n\n'
                      '📚 Просто напишите свой вопрос, и получите мгновенный ответ! Будь то информация о дедлайнах, '
                      'советы по подготовке к экзаменам или ресурсы для изучения — наш бот всегда готов помочь.\n\n'
                      '🎓 Учитесь с умом и не тратьте время на поиски информации — доверьтесь "Студенческому Помощнику!"',
-                     reply_markup=markup)
+                     reply_markup=keyboard)
 
-# крч этих штук можно много создавать и они все отвечают за какие-то команды
-# @bot.message_handler(commands=['help'])
-# def main(message):
-#     bot.send_message(message.chat.id, 'Информация о помощи')
 
 
 @bot.message_handler(content_types=['text'])
@@ -37,6 +33,39 @@ def get_text_messages(message):
         # отправляем изображение с корпусами
         with open('корпус.jpg', 'rb') as photo:
             bot.send_photo(message.chat.id, photo)
+    elif message.text == "Справка":
+        # Пишем приветствие
+        # bot.send_message(message.from_user.id, "Какую информацию вы хотите получить?")
+        # Готовим кнопки
+        keyboard = types.InlineKeyboardMarkup()
+        # По очереди готовим текст и обработчик для каждого вопроса
+        key_obshaga = types.InlineKeyboardButton(text='Общежитие', callback_data='spravka')
+        # И добавляем кнопку на экран
+        keyboard.add(key_obshaga)
+        key_1 = types.InlineKeyboardButton(text='Получить студенческий билет, зачетную книжку, кампусную карту, сдать аттестат ', callback_data='spravka')
+        keyboard.add(key_1)
+        key_2 = types.InlineKeyboardButton(text='Узнать свою группу', callback_data='spravka')
+        keyboard.add(key_2)
+        key_3 = types.InlineKeyboardButton(text='Как попасть в корпус', callback_data='spravka')
+        keyboard.add(key_3)
+        key_4 = types.InlineKeyboardButton(text='PRE-курс', callback_data='spravka')
+        keyboard.add(key_4)
+        key_5 = types.InlineKeyboardButton(text='Личный кабинет ТИУ, Educon', callback_data='spravka')
+        keyboard.add(key_5)
+        key_6 = types.InlineKeyboardButton(text='Коммуникации (беседа ВК, ТГ)', callback_data='spravka')
+        keyboard.add(key_6)
+        key_7 = types.InlineKeyboardButton(text='Перевод, отчисление, восстановление, академический отпуск', callback_data='spravka')
+        keyboard.add(key_7)
+        key_8 = types.InlineKeyboardButton(text='Справки', callback_data='spravka')
+        keyboard.add(key_8)
+        key_9 = types.InlineKeyboardButton(text='Стипендия', callback_data='spravka')
+        keyboard.add(key_9)
+        key_10 = types.InlineKeyboardButton(text='Повышенная стипендия', callback_data='spravka')
+        keyboard.add(key_10)
+        key_11 = types.InlineKeyboardButton(text='Расписание ', callback_data='spravka')
+        keyboard.add(key_11) 
+         # Показываем все кнопки сразу и пишем сообщение о выборе
+        bot.send_message(message.from_user.id, text='Какую информацию вы хотите получить?' ,reply_markup=keyboard)
     elif message.text == "❓️Задать вопрос":
         bot.send_message(message.chat.id,
                          "Пожалуйста, напишите ваш вопрос, и я постараюсь на него ответить.")
@@ -47,6 +76,7 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id,
                          "Я тебя не понимаю. Напиши /help.")
 
+# Запускаем постоянный опрос бота в Телеграме
+bot.polling(none_stop=True, interval=0)
 
-# чтобы ботик не останавливался, без этого сообщение не выводит
-bot.polling(none_stop=True)
+###
