63b31d9503bb0a5a48880aa7c90c7ad3248873db
PolyantsevEgor <144607954+PolyantsevEgor@users.noreply.github.com>
2024-11-11 02:24:59 +0500

    миграция

--- app/api/models.py
+++ app/api/models.py
@@ -16,44 +16,27 @@ class User(Base):
     applications: Mapped[list["Application"]] = relationship(back_populates="user")
 
 
-class Master(Base):
-    __tablename__ = 'masters'
+class Admin(Base):
+    __tablename__ = 'admins'
 
-    master_id: Mapped[int] = mapped_column(Integer, primary_key=True,
-                                           autoincrement=True)  # Уникальный идентификатор мастера
-    master_name: Mapped[str] = mapped_column(String, nullable=False)  # Имя мастера
+    admin_id: Mapped[int] = mapped_column(Integer, primary_key=True,
+                                           autoincrement=True)  # Уникальный идентификатор админа
+    admin_name: Mapped[str] = mapped_column(String, nullable=False)  # Имя админа
 
-    # Связь с заявками (один мастер может иметь несколько заявок)
-    applications: Mapped[list["Application"]] = relationship(back_populates="master")
-
-
-class Service(Base):
-    __tablename__ = 'services'
-
-    service_id: Mapped[int] = mapped_column(Integer, primary_key=True,
-                                            autoincrement=True)  # Уникальный идентификатор услуги
-    service_name: Mapped[str] = mapped_column(String, nullable=False)  # Название услуги
-
-    # Связь с заявками (одна услуга может быть частью нескольких заявок)
-    applications: Mapped[list["Application"]] = relationship(back_populates="service")
+    # Связь с заявками (один админ может иметь несколько заявок)
+    applications: Mapped[list["Application"]] = relationship(back_populates="flvby")
 
 
 class Application(Base):
     __tablename__ = 'applications'
 
-    class GenderEnum(enum.Enum):
-        male = "Мужской"
-        female = "Женский"
-
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)  # Уникальный идентификатор заявки
     user_id: Mapped[int] = mapped_column(BigInteger, ForeignKey('users.telegram_id'))  # Внешний ключ на пользователя
-    master_id: Mapped[int] = mapped_column(Integer, ForeignKey('masters.master_id'))  # Внешний ключ на мастера
-    service_id: Mapped[int] = mapped_column(Integer, ForeignKey('services.service_id'))  # Внешний ключ на услугу
+    admin_id: Mapped[int] = mapped_column(Integer, ForeignKey('masters.master_id'))  # Внешний ключ на админа
     appointment_date: Mapped[Date] = mapped_column(Date, nullable=False)  # Дата заявки
     appointment_time: Mapped[Time] = mapped_column(Time, nullable=False)  # Время заявки
-    gender: Mapped[GenderEnum] = mapped_column(Enum(GenderEnum), nullable=False)
-    client_name: Mapped[str] = mapped_column(String, nullable=False)  # Имя пользователя
-    # Связи с пользователем, мастером и услугой
+
+    # Связи с админом, пользователем, заявкой
     user: Mapped["User"] = relationship(back_populates="applications")
-    master: Mapped["Master"] = relationship(back_populates="applications")
-    service: Mapped["Service"] = relationship(back_populates="applications")
+    master: Mapped["Admin"] = relationship(back_populates="applications")
+    service: Mapped["Application"] = relationship(back_populates="applications")
