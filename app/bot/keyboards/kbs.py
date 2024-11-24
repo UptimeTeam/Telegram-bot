d15c62cd0b7db63fcc6c840b96b04d1b8f0645b9
PolyantsevEgor <144607954+PolyantsevEgor@users.noreply.github.com>
2024-11-25 03:30:34 +0500

    непон

---
+++ app/bot/keyboards/kbs.py
@@ -0,0 +1,39 @@
+from aiogram.types import ReplyKeyboardMarkup, WebAppInfo, InlineKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+from app.config import settings
+
+
+def main_keyboard(user_id: int, first_name: str) -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    url_add_application = f'{settings.BASE_SITE}/form?user_id={user_id}&first_name={first_name}'
+    kb.button(text="❓️Задать вопрос")
+    kb.button(text="ℹ️ О нас")
+    kb.button(text="🔍Часто задаваемые вопросы")
+    if user_id == settings.ADMIN_ID:
+        kb.button(text="🔑 Админ панель")
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
+
+
+def back_keyboard() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="🔙 Назад")
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
+
+
+def admin_keyboard(user_id: int) -> InlineKeyboardMarkup:
+    url_applications = f"{settings.BASE_SITE}/admin?admin_id={user_id}"
+    kb = InlineKeyboardBuilder()
+    kb.button(text="📝 Смотреть заявки", web_app=WebAppInfo(url=url_applications))
+    kb.adjust(1)
+    return kb.as_markup()
+
+
+def app_keyboard(user_id: int, first_name: str) -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    url_add_application = f'{settings.BASE_SITE}/form?user_id={user_id}&first_name={first_name}'
+    kb.button(text="📝 Оставить заявку", web_app=WebAppInfo(url=url_add_application))
+    kb.adjust(1)
+    return kb.as_markup()
